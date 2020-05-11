b743df8b3880c7997a5ed1654867cf51164584d4
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2020-05-11 10:15:30 +0200

    provide spring-boot project :construction:

---
+++ covivre/src/main/java/fr/istv/covivre/covivre/CovivreApplication.java
@@ -0,0 +1,13 @@
+package fr.istv.covivre.covivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovivreApplication.class, args);
+	}
+
+}
---
+++ covivre/src/test/java/fr/istv/covivre/covivre/CovivreApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.istv.covivre.covivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
