d86048674fb9973fc4f06191b1e5c867cf9257ba
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2020-05-14 15:56:50 +0200

    fix build

--- covivre-front/src/app/uuid.service.ts
+++ covivre-front/src/app/uuid.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,15 @@ export class UuidService {
 
   constructor(public http: HttpClient) { }
 
-  getUUID() {
+  getUUID(): Observable<any> {
     return this.http.get('https://localhost:8080/uuid/');
   }
 
-  getTemporaryToken(uuid: string) {
+  getTemporaryToken(uuid: string): Observable<any>  {
     return this.http.post<any>('https://localhost:8080/uuid/temporary-token', {	uuid});
   }
 
-  alertUsers(temporarytokens: any[]) {
+  alertUsers(temporarytokens: any[]): Observable<any> {
     return this.http.post<any>('https://localhost:8080/users/alert-users', {	uuids : temporarytokens.map(t => t.token)});
   }
 
